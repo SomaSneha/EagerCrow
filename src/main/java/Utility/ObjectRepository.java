93bca4f6198f103e003669a5fe9fd860942cb87e
Ritu <Ritu>
2020-01-29 22:34:05 +0530

    changes done

--- src/main/java/Utility/ObjectRepository.java
+++ src/main/java/Utility/ObjectRepository.java
@@ -6,21 +6,18 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ObjectRepository {
-protected String getLocator(String l) {
-	Properties prop=new Properties();
-	try{
-		FileInputStream fis=new FileInputStream("KeywordDrivenFramework/Config/ObjectRepository.properties");
-	    prop.load(fis);
-	    String locator=(String) prop.get(l);
-	    
-	}
-	catch(FileNotFoundException e) {
-		e.getStackTrace();
-	}
-	catch(IOException e) {
-		e.getStackTrace();
-	}
-	return l;
+protected String getLocator(String l) throws IOException {
+	
+	Properties prop = new Properties();
+
+	
+		FileInputStream fis = new FileInputStream("KeywordDrivenFramework/Config/ObjectRepository.properties");
+		prop.load(fis);
+		String locator = prop.getProperty(l);
+	
+	
+	return locator;
+	
 	
 
 }
